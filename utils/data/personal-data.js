07d5b032489efa340ab58d545aed3fb1b8babed0
Navanetha_Krishnan_14 <113086797+NavanethaKrishnan-14@users.noreply.github.com>
2026-03-24 20:27:01 +0530

    Change resume link in personal-data.js
    Updated resume link to a new Google Drive URL.

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -11,5 +11,5 @@ export const personalData = {
   facebook: "",
   linkedIn: "https://www.linkedin.com/in/navanetha-krishnan-s-71143126a",
   resume:
-    "https://drive.google.com/file/d/1upHi108NWQZi8ngirGtYJoovfBWm40_B/view?usp=drive_link",
+    "https://drive.google.com/file/d/1AwJZgCl7BV9k0Y9QTEbllu6WdGmJJRb8/view?usp=drive_link",
 };

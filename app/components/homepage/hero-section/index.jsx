c6558043c33be84a7770718eee19125f9cc6fbb8
Navanetha_Krishnan_14 <113086797+NavanethaKrishnan-14@users.noreply.github.com>
2026-03-24 20:25:27 +0530

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -65,7 +65,6 @@ function HeroSection() {
             </Link>
 
             <Link
-              href="https://drive.google.com/file/d/1AwJZgCl7BV9k0Y9QTEbllu6WdGmJJRb8/view?usp=drive_link"
               className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
               role="button"
               target="_blank"
